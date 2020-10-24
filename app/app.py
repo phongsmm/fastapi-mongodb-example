d14c17cdf3f45e597a13cbd49a373bc156afbf65
PhongpisutMM <69627422+PhongpisutMM@users.noreply.github.com>
2020-10-24 14:10:46 +0700

    adding list calculate

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
 from typing import Optional
 from fastapi import FastAPI , Body
 from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
 from decouple import config
 from pymongo import MongoClient
 from pydantic import BaseModel
 from bson import ObjectId
+
+
 
 
 
@@ -15,6 +18,14 @@ class Ner(BaseModel):
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=config('ORIGINS'),
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 MONGO_DETAILS = config('MONGO_DETAILS')
 client = MongoClient(MONGO_DETAILS)
 db = client['Ner']
@@ -39,3 +50,12 @@ async def add(req : Ner = Body(...)):
     db.tags.insert_one(dict(req))
     return dict(req);
 
+@app.get("/list/")
+async def all_list():
+    person = db.tags.find({"mentions.type":"Person"}).count()
+    location = db.tags.find({"mentions.type":"Location"}).count()
+    number = db.tags.find({"mentions.type":"Number"}).count()
+
+    return {"Person":person , "Location":location,"Number":number}
+
+    

dbcd157bb3c359e349e4af1ad63f9d397dd801f5
PhongpisutMM <69627422+PhongpisutMM@users.noreply.github.com>
2020-10-24 18:29:52 +0700

    fix error

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: uvicorn app:app
+web: py main.py
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import uvicorn
+from decouple import config
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000||config('PORT'), reload=True)

20c2e79124fa429d9b3db1c41fba608e7df4fd1e
PhongpisutMM <69627422+PhongpisutMM@users.noreply.github.com>
2020-10-26 01:06:10 +0700

    v2.0

--- app.py
+++ app.py
@@ -17,6 +17,29 @@ class Ner(BaseModel):
     mentions : list
 
 
+class Ner_update(BaseModel):
+    text : str
+    mentions : list
+
+class Location(BaseModel) :
+    begin : int
+    end : int
+class Mention_update(BaseModel):
+
+    location: Location
+    text :str
+    type : str
+
+
+    
+
+class Type_update(BaseModel):
+
+    type : str
+
+
+
+
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -55,7 +78,35 @@ async def all_list():
     person = db.tags.find({"mentions.type":"Person"}).count()
     location = db.tags.find({"mentions.type":"Location"}).count()
     number = db.tags.find({"mentions.type":"Number"}).count()
+    
 
     return {"Person":person , "Location":location,"Number":number}
 
     
+@app.put("/update/{url_id}")
+async def update(url_id:int,req : Ner_update = Body(...)):
+    if(db.tags.find({"id":url_id}).count()>0) :
+        db.tags.update_one({"id":url_id},{"$set":dict(req)})
+        return dict(req)
+        
+    else :
+        return{"Alert":"ID NOT FOUND!"}
+
+
+@app.delete("/unset/{url_id}/")
+async def unset(url_id:int,req: Mention_update = Body(...)):
+    if(db.tags.find({"id":url_id}).count()>0):
+        db.tags.update_one({"id":url_id},{"$pull":{"mentions":{'$in':[dict(req)]}}})
+        return dict(req)
+    else :
+        return{"Alert":"ID NOT FOUND!"}
+
+
+@app.put("/settype/{url_id}/{begin}/{end}")
+async def settype(url_id:int,begin:int,end:int,req:Type_update = Body(...)):
+    if(db.tags.find({"id":url_id}).count()>0):
+        db.tags.update_one({"id":url_id,"mentions.location.begin":begin,"mentions.location.end":end},{'$set':{"mentions.$.type":dict(req)}})
+        return dict(req)
+    else :
+        return{"Alert":"ID NOT FOUND!"}
+
--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from decouple import config
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0",port="8000", reload=True)
+    uvicorn.run("app:app", host="0.0.0.0",port=8000, reload=True)

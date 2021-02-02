d47b5171b3bed2eaaa8f5a3633cf887698a2cd53
Phongpisut Meemuk <me.phongpisut_st@tni.ac.th>
2021-02-02 14:32:46 +0700

    Admin

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from pymongo import MongoClient
 from pydantic import BaseModel , Field
 from bson import ObjectId
 from datetime import datetime
-
-
+from asynchat import async_chat
 
 
 class PyObjectId(ObjectId):
@@ -43,7 +42,14 @@ class Add_Event(BaseModel):
     Text: str
     Date: Optional[datetime] = datetime.now()
 
+class Add_News(BaseModel):
+    Text: str
+    Img_url:str
+    Date: Optional[datetime] = datetime.now()
 
+class Users(BaseModel):
+    Username:str
+    Password:str
 
 app = FastAPI()
 app.add_middleware(
@@ -60,7 +66,7 @@ db = client['Wattheplela']
 
 @app.get("/")
 def home():
-    return {"message": "ยินดีต้อนรับเข้าสู่ API ของวัดเทพลีลา"}
+    return {"message": "ยินดีต้อนรับ"}
 
 @app.get("/get/")
 async def show():
@@ -73,5 +79,27 @@ async def show():
 @app.post('/event')
 async def add_event(event: Add_Event):
     ret = db.Data.insert_one(event.dict(by_alias=True))
-    return {'user': event}
+    return {'Event': event}
+
+@app.post('/news')
+async def add_news(news:Add_News ):
+    ret = db.Data.insert_one(news.dict(by_alias=True))
+    return {'News':news}
+
+@app.post('/checkAdmin')
+async def check_admin(user:Users):
+    username = []
+    password = []
+    for e,i in enumerate(db.Users.find()):
+        username.append(i['Username'])
+        password.append(i['Password'])
+    if user.Username in username:
+        if user.Password == password[username.index(user.Username)]:
+            return {'Status':'Successful'}
+        else:
+            return {'Status':'Access Denied'}
+    else :
+        return {'Status':'Access Denied'}
+
+
 
